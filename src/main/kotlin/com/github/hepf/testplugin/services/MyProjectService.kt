3b3e06e6288858704fa916a5546278e82a701a55
GitHub Action <action@github.com>
2022-03-29 09:41:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/hepf/testplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/hepf/testplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.hepf.testplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.hepf.testplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
